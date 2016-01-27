db11d5771d00eef6c55711cf42c7927df452a301
meng-sun <m-sun@live.com>
2016-01-27 18:46:43 -0500

    Create StackGet.cpp

---
+++ src/app/StackGet.cpp
@@ -0,0 +1,25 @@
+#include "CV.h"
+#include "StackGet.h"
+#include <iostream>
+using namespace std;
+
+bool getStack(int argc, char **argv) {
+    //Move towards stack
+    Camera cam;
+	while (true) {
+		cam.moveTowardsStack();
+		if(cam.stackFound == 1) //If stack is found
+        {
+            //Move towards individual cubes
+            while (true) {
+                cam.moveTowardsCube();
+                if ((cam.cubeFound >= 3) || (cam.cubeNotFound > 100) //If 3 cubes are found or timeout
+                    return true;
+            }
+
+        }
+        else if(cam.stackNotFound > 100) //time out
+            return false;
+
+	}
+}
