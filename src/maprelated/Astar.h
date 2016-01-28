d569b74e5db4e56b7a1dec0d1bc637d9c7fef5c6
meng-sun <m-sun@live.com>
2016-01-27 22:30:21 -0500

    Update Astar.h

--- src/maprelated/Astar.h
+++ src/maprelated/Astar.h
@@ -1,13 +1,10 @@
+#ifndef _ASTAR
+#define _ASTAR
 /*
  Sample code from http://www.redblobgames.com/pathfinding/
  Copyright 2014 Red Blob Games <redblobgames@gmail.com>
  License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 */
-
-//include time out stuff
-//start in the middle of the map
-//one of the problems is with currentBounds.
-//I can set it really big but it has to match up with the probabilitymap array 
 
 #include <iostream>
 #include <iomanip>
@@ -40,8 +37,7 @@ struct Grid {
   typedef tuple<int,int> Location;
   Location (&allLocations)[currentBounds][currentBounds]; 
   
-  //for testing
-  string movementArray[currentBounds][currentBounds];
+
   Grid(Location (&locs)[currentBounds][currentBounds]): allLocations(locs) {}
   inline bool in_bounds(Location id) const {
   	int x, y;
@@ -69,60 +65,24 @@ struct Grid {
     std::reverse(results.begin(), results.end());
     return results;
   }
-  
-  //for testing
-  void addGridMovement(Location from, Location to);
-  void drawFinalGrid() {
- 
- 	std::cout << "drawing final grid" << std::endl;
-    	int i = 0;
-    	int j = 0;
-  		while (i <currentBounds) {
-  			std::cout <<  "  "<< i+1 << "-" ;
-  			++i;
-  		}
-  
-  		while (j< currentBounds) {
-  			int k = 0;
-  			std::cout << "\n";
-  			std::cout << j+1 <<"|" ;
-  			while (k<currentBounds) {
-  				if (movementArray[k][j]!="") {
-  					std::cout << " " << movementArray[k][j] << "  ";
-  				} else {
-  					std::cout << " .  "  ;
-  				}
-  				++k;
-  			}
-  			++j;
-  		}
-  	std::cout << "\n";
- } 
 };
 
 namespace std{
 typedef typename Grid::Location Location;
-template <> struct std::hash<Location> {
-    std::size_t operator()(const Location k) const
+template <> struct hash<Location> {
+    size_t operator()(const Location k) const
     {
     int x, y;
 	tie (x, y) = k;
-      // Compute individual hash values for first,
-      // second and third and combine them using XOR
-      // and bit shifting:
-
-      return x*524287+y*37;
+    return x*524287+y*37;
     }
   };
 }
-
-//for testing
-void Grid::addGridMovement(Location from, Location to) {}
   
 template<typename Location>
-vector<Location> reconstruct_path(Grid grid, Location start, Location goal, unordered_map<Location, Location> &came_from) {}
+vector<Location> reconstruct_path(Grid grid, Location start, Location goal, unordered_map<Location, Location> &came_from);
 
-inline int heuristic(Grid::Location a, Grid::Location b) {}
+inline int heuristic(Grid::Location a, Grid::Location b);
 
 template<typename T, typename Number=int>
 struct PriorityQueue {
@@ -143,10 +103,7 @@ struct PriorityQueue {
   }
 };
 
-vector<Grid::Location> a_star_search
-  (Grid grid,
-   typename Grid::Location start,
-   typename Grid::Location goal,
-   int probabilityMap[10][10] //bounds must match
-   ) {}
+
+vector<Grid::Location> astarsearch(Grid grid, Grid::Location start, Grid::Location goal, int probabilityMap[10][10]);
   
+#endif
