f0f50d5543bf17ec729edbaf0ff2d7f9f606f62a
Alan Cheng <adcheng@mit.edu>
2016-01-29 10:23:54 -0500

    Last effort

---
+++ src/app/GameProgram.cpp
@@ -0,0 +1,39 @@
+#include "../CV.h"
+#include "../StackGet.h"
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+#include "../HAL.h"
+using namespace std;
+{
+    Camera cam;
+    while (true)
+    {
+        srand (time(NULL));
+        cam.stackNotFound = 0;
+        cam.stackFound = 0;
+        cam.cubeNotFound = 0;
+        cam.cubeFound = 0;
+
+        cam.gameMode = 0; // 0 = red; 1 = green
+
+        while (true)
+        {
+            cam.moveTowardsStack();
+            int v1 = rand() % 1000;
+            if((cam.stackFound >= 1)||(cam.stackNotFound >= 1000)||(v1==1000))
+               break;
+        }
+        if(cam.stackFound >= 1)
+        {
+            while (true)
+            {
+                cam.moveTowardsCube();
+                if(cam.cubeNotFound >= 1000)
+                    break;
+            }
+        }
+
+        randomMovement();
+    }
+}
